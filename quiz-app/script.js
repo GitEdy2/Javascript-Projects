303e23c4a0f9b25b9528f146cd76bc702e552d6f
Edison Proano <clanuzumaki22@gmail.com>
2020-09-30 21:53:54 -0500

    Creacion de conjunto de preguntas y respuestas.

---
+++ quiz-app/script.js
@@ -0,0 +1,46 @@
+const quizData = [
+    {
+        question: 'How old is Florin?',
+        a: '10',
+        b: '20',
+        c: '30',
+        d: '40',
+        correct: 'c'
+    },
+
+    {
+        question: 'What is the programing language in 2019?',
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'Javascript',
+        correct: 'd'
+    },
+    {
+        question: 'Who is the president of US?',
+        a: 'Florin Pop',
+        b: 'Donald Trump',
+        c: 'Ivan Saldano',
+        d: 'Mihai Andrei',
+        correct: 'b'
+    },
+    {
+        question: 'What does HTML stand for?',
+        a: 'Hypertextual Markup Language',
+        b: 'Cascading Style Sheet',
+        c: 'Jason Object Natation',
+        d: 'Helicopters Terminals Motorboats Lamborginis',
+        correct: 'a'
+    },
+    {
+        question: 'What year was Javascript launched?',
+        a: '2020',
+        b: '2019',
+        c: '2018',
+        d: 'none of the above',
+        correct: 'd'
+    },
+
+
+
+]
